a9de1a644e1342503bcd53442cf573cf9ac32f6d
jordan.lafland <j.t.lafland@gmail.com>
2017-08-06 21:11:48 -0400

    add working pp copy

---
+++ app/horizontal-responsive-master 2/README.md
@@ -0,0 +1,13 @@
+# horizontal-responsive
+horizontal responsive website template
+
+
+CodePen Template/Demo:
+https://codepen.io/lafland/pen/aJvzxP
+
+Live location:
+https://laf.land/resume/visual/
+https://jordan.bz/resume/visual/
+
+
+Internet Archive:  February 26, 2017. https://web-beta.archive.org/details/https://laf.land/resume/visual
---
+++ app/marina/script/extra.js
@@ -0,0 +1,215 @@
+// The window object implements the Window interface, which in turn inherits from the AbstractView global functions, namespaces, objects, interfaces, and constructors,available on it, 
+// offline resources
+// interesting properties:
+
+// Window.innerHeight
+// Window.innerWidth
+// Window.isSecureContext 
+// Window.location
+// Window.navigator
+// Window.opener
+// Window.status
+
+
+
+$(".open-video").trigger("click");
+$(".play-icon").trigger("click");
+
+$("#cphMasterContent_cphPageContent_rptSections_pnLeftCol_0 > div.video-container > div.video-overlay > a").click();
+
+$("#form1 > div.stories-slider > ul > div > div > li.slick-slide.slick-current.slick-active > div > button.open-video").click();
+
+
+
+$('#patientVideoPlayer').get(0).scrollIntoView(true);
+window.scrollBy(0, -150);
+
+
+$("#cphMasterContent_maincontent_0_rbJourneyGoForward").click();
+
+screenshooter.utility.showDropDowns("#cphMasterContent_maincontent_0_ddlStartDateMonth, #cphMasterContent_maincontent_0_ddlStartDateYear, #cphMasterContent_maincontent_0_ddlStartDateDay");
+
+$("#patientVideoPlayer > div.VideoPlayer_videos.slick-initialized.slick-slider > div > div > div.VideoPlayer_video.video_mitch.slick-slide.slick-active > a").click();
+
+
+#patientVideoPlayer > div.VideoPlayer_videos.slick-initialized.slick-slider > div > div > div.VideoPlayer_video.video_mitch.slick-slide.slick-active > a
+
+
+$("#form1 > div.stories-slider > ul > div > div > li.slick-slide.slick-current.slick-active > div > div.story-copy > h3 > button").click();
+
+$("#cphMasterContent_maincontent_0_rblPresPaid_1").click();
+$("#cphMasterContent_maincontent_0_rblAckIneligible_0").click();
+$("#cphMasterContent_maincontent_0_rblResident_0").click();
+$("#cphMasterContent_maincontent_0_cbRegisterSupport").click();
+$("#cphMasterContent_maincontent_0_rbJourneyGetReady").click();
+$("#cphMasterContent_maincontent_0_rblPhoneCheckins_0").click();
+$("#cphMasterContent_maincontent_0_rblInsuranceProcess_4").click();
+
+$(".open-video").click();
+
+$("#form1 > div.stories-slider > ul > div > div > li.slick-slide.slick-current.slick-active > div > button.open-video").click();
+$(".open-video").trigger("click");
+
+$('.open-video').click(),2000;
+
+$( "#patientVideoPlayer > div.VideoPlayer_videos.slick-initialized.slick-slider > div > div > div.VideoPlayer_video.video_mitch.slick-slide.slick-active.active > a" ).addClass( "active" );
+$( "#patientVideoPlayer > div.VideoPlayer_videos.slick-initialized.slick-slider > div > div > div.VideoPlayer_video.video_mitch.slick-slide.slick-active.active" ).addClass( "active" );
+#patientVideoPlayer > div.VideoPlayer_videos.slick-initialized.slick-slider > div > div > div.VideoPlayer_video.video_mitch.slick-slide.slick-active.active
+
+
+#patientVideoPlayer > div.VideoPlayer_videos.slick-initialized.slick-slider > div > div > div.VideoPlayer_video.video_mitch.slick-slide.slick-active.active > a
+
+
+$(".play-icon").trigger("click");
+
+$("video-overlay").show();
+
+
+setTimeout(function(){ }, 3000);
+
+
+
+
+
+$('#treatment-three').get(0).scrollIntoView(true);
+window.scrollBy(0, -100);
+
+$('#treatment-three').get(0).scrollIntoView({block: "end", behavior: "smooth"});
+
+$('#cphMasterContent_cphPageContent_rptSections_pnLeftCol_2').get(0).scrollIntoView(false);
+
+
+$('#cphMasterContent_cphPageContent_rptSections_pnLeftCol_0').get(0).scrollIntoView(false);
+window.scrollBy(0, -300);
+
+$('#testimonial').get(0).scrollIntoView(true);
+window.scrollBy(0, -100);
+video-container
+
+$('.VideoPlayer_video').attr('style','display:none');
+$('.video-container').attr('style','display:none');
+$('#patientVideoPlayer').attr('style','display:block');
+
+
+$('#cphMasterContent_cphPageContent_rptSections_pnLeftCol_1 > div > button.directory-button.charlotte.right-arrow').click();
+
+$('.favorite').attr('class','favorite liked');
+
+
+$('.top-copy').attr('style','display:none');
+$('.lantern').attr('style','display:none');
+$('.lantern-release').attr('style','display:none');
+
+$("#top > main > section > div > div.left-col > fieldset > div:nth-child(4) > label").trigger("click");
+
+
+$('.errorMsg').attr('style','display:block');
+$('label').attr('class','error');
+
+
+$("#top > main > section > div > div.left-col > fieldset > div:nth-child(4) > label").trigger("click");
+
+$("#cphMasterContent_maincontent_0_rbJourneyGetReady").click()
+
+
+cphMasterContent_maincontent_0_ddlStartDateMonth
+cphMasterContent_maincontent_0_ddlStartDateDay
+cphMasterContent_maincontent_0_ddlStartDateYear
+
+screenshooter.utility.showDropDowns("#cphMasterContent_maincontent_0_ddlStartDateMonth, #cphMasterContent_maincontent_0_ddlStartDateDay, #cphMasterContent_maincontent_0_ddlStartDateYear");
+
+$("#cphMasterContent_maincontent_0_rblPresPaid_1").click();
+$("#cphMasterContent_maincontent_0_rblAckIneligible_0").click();
+$("#cphMasterContent_maincontent_0_rblResident_0").click();
+$("#cphMasterContent_maincontent_0_cbRegisterSupport").click();
+$("#cphMasterContent_maincontent_0_rbJourneyGetReady").click();
+$("#cphMasterContent_maincontent_0_rbJourneyGetReady").click()
+
+$("#cphMasterContent_maincontent_0_rbJourneyGoForward").click();
+
+
+screenshooter.utility.showDropDowns("#cphMasterContent_cphPageContent_maincontent_0_Label1, #cphMasterContent_cphPageContent_maincontent_0_ddlStateRes");
+
+$("#cphMasterContent_cphPageContent_maincontent_0_rfvStateRes").show();
+
+$("#cphMasterContent_cphPageContent_maincontent_0_rfvCureDate").show();
+
+$(".errorMsg").show();
+$(".errorMsg .validatorContact").show();
+
+$('select').attr('style','border-color:red');
+$('input').attr('style','border-color:red');
+$("#cphMasterContent_cphPageContent_maincontent_0_rfvEmail").attr('style','display:block');
+$("#cphMasterContent_cphPageContent_maincontent_0_evEmail").attr('style','display:block');
+
+$("#cphMasterContent_cphPageContent_maincontent_0_rfvFirstName").attr('style','display:block');
+$("#cphMasterContent_cphPageContent_maincontent_0_regexFirstName").attr('style','display:block');
+
+
+
+#cphMasterContent_maincontent_0_cbRegisterSupport
+
+#cphMasterContent_maincontent_0_rblResident > label.label_check.c_on
+#cphMasterContent_maincontent_0_rblPresPaid > label.label_check.c_on > span
+#cphMasterContent_maincontent_0_rblAckIneligible
+
+$("#cphMasterContent_maincontent_0_rblResident, #cphMasterContent_maincontent_0_rblPresPaid, #cphMasterContent_maincontent_0_rblAckIneligible").click();
+
+
+#top > main > section > div > div.left-col > fieldset > div:nth-child(4) > label
+
+
+$("#top > main > section > div > div.left-col > fieldset > div:nth-child(4) > label").trigger("click");
+
+
+$("#cphMasterContent_cphPageContent_maincontent_0_ddlMonth").click();
+$("#cphMasterContent_cphPageContent_maincontent_0_ddlDay").trigger("click");
+$("#cphMasterContent_cphPageContent_maincontent_0_ddlYear").trigger("click");
+$("#cphMasterContent_cphPageContent_maincontent_0_tbDisplayInitials").trigger("click");
+$("#cphMasterContent_cphPageContent_maincontent_0_tbEmail").trigger("click");
+$("#cphMasterContent_cphPageContent_maincontent_0_tbFirstName").trigger("click");
+$("#cphMasterContent_cphPageContent_maincontent_0_tbLastName").trigger("click");
+$(".label_checkbox").trigger("click");
+
+$("#cphMasterContent_cphPageContent_maincontent_0_ddlMonth").click();
+$("#cphMasterContent_cphPageContent_maincontent_0_ddlDay").trigger("click");
+$("#cphMasterContent_cphPageContent_maincontent_0_ddlYear").trigger("click");
+$("#cphMasterContent_cphPageContent_maincontent_0_tbDisplayInitials").trigger("click");
+$("#cphMasterContent_cphPageContent_maincontent_0_tbEmail").trigger("click");
+$("#cphMasterContent_cphPageContent_maincontent_0_tbFirstName").trigger("click");
+$("#cphMasterContent_cphPageContent_maincontent_0_tbLastName").trigger("click");
+$(".label_checkbox").trigger("click");
+
+$("#cphMasterContent_cphPageContent_maincontent_0_rfvStateRes").show();
+
+$("#cphMasterContent_cphPageContent_maincontent_0_rfvCureDate").show();
+
+$("#cphMasterContent_cphPageContent_maincontent_0_rfvCureFeeling").show();
+
+
+
+
+
+
+
+
+$("#InitErr").show();
+
+video-container
+1000
+650
+
+$('.video-play').click(),2000;
+$('#testimonial').get(0).scrollIntoView(true);
+window.scrollBy(0, -100);
+$('.video_mitch').click();
+
+$('.video-play').click(),2000;
+$('#testimonial').get(0).scrollIntoView(true);
+window.scrollBy(0, -100);
+$('.video_carol').click();
+
+$('.video-play').click(),2000;
+$('#testimonial').get(0).scrollIntoView(true);
+window.scrollBy(0, -100);
+$('.video_commercial').click();
